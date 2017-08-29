5341c5f1481179b3ea75d215e74411840331eb37
Andrew Pratt <apratt0303@gmail.com>
2017-08-29 11:28:01 -0400

    Updated exp.R
    Added notes to the code file

--- exp.R
+++ exp.R
@@ -1,9 +1,17 @@
+mean(means) # mean of samples of size 40
+1/lambda    # theoretical mean of samples of size 40
+sd(means)   # sd of samples of size 40
+1/lambda/sqrt(40) # theoretical SE of samples of size 40
+
+
 
 set.seed(333)
 lambda <- .2
 n <- 40
 nsims <- 1000
 
+# exp.sim() is a function that generates means of samples of
+# 40 random exponentials
 exp.sim <- function(nsims, n, lambda){
         means <- numeric()
         for(i in 1:nsims){
@@ -12,18 +20,26 @@ exp.sim <- function(nsims, n, lambda){
         means
 }
 
-means <- exp.sim(nsims, n, lambda)
-sim.mean <- mean(means)
-sim.sd <- sd(means)
-sim.range <- range(means)
+
+means <- exp.sim(nsims, n, lambda) # create distribution of sample means
+sim.mean <- mean(means) # calculate mean of sample means
+sim.sd <- sd(means) # sd of sample means
+sim.range <- range(means) # range of sample means
+
+# Create vector of normal density values using sim.mean and sim.sd
+# to plot on top of histogram
 sequence <- seq(sim.range[1], sim.range[2], length = 1000)
 norm.curve <- dnorm(sequence, sim.mean, sim.sd)
 
+# (ggplot annotation information)
 xblack <- c(sim.mean + .05, sim.mean + .5, sim.mean + .55)
 xred <- c(1/lambda - .05, 1/lambda - .5, 1/lambda - .55)
 yblack <- yred <- max(norm.curve) + .05
 
 
+# Plot a histogram of the sample means with the normal density curve overlayed on
+# top. Plot vertical lines showing the observed mean of the simulated samples and
+# the theoretical mean of samples of size 40.
 library(ggplot2)
 ggplot() + geom_histogram(aes(means, ..density..), color = "black", fill = "black", alpha = .3) +
         geom_line(aes(sequence, norm.curve), size = 1.5, color = "black") +
@@ -67,7 +83,3 @@ ggplot() + geom_histogram(aes(means, ..density..), color = "black", fill = "blac
               axis.text = element_text(size = 12),
               axis.title = element_text(size = 15))
 
-mean(means) # mean of samples of size 40
-1/lambda    # theoretical mean of samples of size 40
-sd(means)   # sd of samples of size 40
-1/lambda/sqrt(40) # theoretical SE of samples of size 40
